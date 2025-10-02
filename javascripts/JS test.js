af09eb9647ab3294577efb5c8758fc2908815459
philip-meie <mixexfp@gmail.com>
2025-10-02 22:28:29 +0700

    Update JS test.js

--- javascripts/JS test.js
+++ javascripts/JS test.js
@@ -1,1 +1,13 @@
-alert("hhrrrrrrrrllll")
+let input = prompt("Input integer number");
+
+let number = parseInt(input);
+
+if (isNaN(number)) {
+    alert("Please enter a valid integer number");
+} else if (number === 0) {
+    alert("zero integer number");
+} else if (number % 2 === 0) {
+    alert("even integer number");
+} else {
+    alert("odd integer number");
+}
